87c03d0cf9bd28117c8677eecd3af90006386486
shmeegie <ericellb@gmail.com>
2019-09-11 02:05:21 -0400

    Counter Server getCount

--- server/src/counterserver/routes.ts
+++ server/src/counterserver/routes.ts
@@ -15,10 +15,27 @@ router.get('/count', async (req: Request, res: Response) => {
     res.status(200).send(startCount);
   }
 });
+
 // Will return the current count of given server
 // If server does not have a count / exceeded their current count
 // Will create a new count range for them
-export const getCount = async (serverPort: string) => {};
+export const getCount = async (serverPort: string) => {
+  // If current non exhausted count exists for given serverId return it
+  let rows: any = await sql.query(`SELECT * from counters WHERE server_id=${sql.escape(serverPort)} AND exhausted='0'`);
+  if (rows[0]) {
+    return { startCount: rows[0].start_count, currentCount: rows[0].current_count };
+  }
+
+  // If no rows for our serverId or all counts for serverId exhausted create a new one
+  rows = await sql.query(`SELECT * FROM counters ORDER BY id DESC LIMIT 1`);
+  const lastId = rows[0].id + 1;
+  rows = await sql.query(
+    `INSERT INTO counters (server_id, start_count, current_count, exhausted) VALUES (${sql.escape(
+      serverPort
+    )}, ${lastId * 1000000}, '0', '0')`
+  );
+  return { startCount: rows.start_count, currentCount: rows.current_count };
+};
 
 // DB SCHEMA FOR Counter Table
 // id - serverId - start_count - current_count - exhausted
