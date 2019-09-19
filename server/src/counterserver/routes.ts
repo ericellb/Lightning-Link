79b00307c3998459487c811b27cb9c9aa6d3504d
shmeegie <ericellb@gmail.com>
2019-09-19 01:16:29 -0400

    Allow to request new count seperatly

--- server/src/counterserver/routes.ts
+++ server/src/counterserver/routes.ts
@@ -5,14 +5,24 @@ import { MysqlError } from 'mysql';
 
 export let router = express.Router();
 
-// Route to get original url given short url
+// Route to get current count of given server (Identified by server port for now)
 router.get('/count', async (req: Request, res: Response) => {
   const { serverPort } = req.query;
   if (!serverPort) {
     res.status(400).send('Must provide a server port / id');
   } else {
-    const startCount = await getCount(serverPort);
-    res.status(200).send(startCount);
+    const counts = await getCount(serverPort);
+    res.status(200).send(counts);
+  }
+});
+
+router.get('/newcount', async (req: Request, res: Response) => {
+  const { serverPort } = req.query;
+  if (!serverPort) {
+    res.status(400).send('Must provide a server port / id');
+  } else {
+    const counts = await newCount(serverPort);
+    res.status(200).send(counts);
   }
 });
 
@@ -26,8 +36,13 @@ export const getCount = async (serverPort: string) => {
     return { startCount: rows[0].start_count, currentCount: rows[0].current_count };
   }
 
+  return newCount(serverPort);
+};
+
+// Gets a new count range
+export const newCount = async (serverPort: string) => {
   // If no rows for our serverId or all counts for serverId exhausted create a new one
-  rows = await sql.query(`SELECT * FROM counters ORDER BY id DESC LIMIT 1`);
+  let rows: any = await sql.query(`SELECT * FROM counters ORDER BY id DESC LIMIT 1`);
   const lastId = rows[0].id + 1;
   let startCount = lastId * 1000000;
   await sql.query(
@@ -37,6 +52,3 @@ export const getCount = async (serverPort: string) => {
   );
   return { startCount: startCount, currentCount: 0 };
 };
-
-// DB SCHEMA FOR Counter Table
-// id - serverId - start_count - current_count - exhausted
