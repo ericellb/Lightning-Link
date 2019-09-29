091cd8c8b4d45a87a945228e6bad840231ed62dc
shmeegie <ericellb@gmail.com>
2019-09-29 00:33:48 -0400

    Appbar Header

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -3,16 +3,56 @@ import Header from '../Header';
 import Hero from '../Hero';
 import Shortener from '../Shortener';
 import Footer from '../Footer';
-import HeaderBar from '../Header';
-import { Container } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import './style.css';
 
 export default function App() {
   return (
-    <Container>
+    <ThemeProvider theme={theme}>
       <Header />
       <Hero />
       <Shortener />
       <Footer />
-    </Container>
+    </ThemeProvider>
   );
 }
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiTooltip: {
+      tooltip: {
+        fontSize: '14px',
+        backgroundColor: 'black'
+      }
+    },
+    MuiSnackbarContent: {
+      root: {
+        backgroundColor: '#202225',
+        color: 'white'
+      }
+    },
+    MuiAppBar: {
+      colorPrimary: {
+        backgroundColor: '#3490de',
+        position: 'absolute'
+      }
+    }
+  },
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#7289da'
+    },
+    secondary: {
+      main: '#3ca374'
+    }
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontSize: 14,
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600
+  }
+});
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, Button, makeStyles } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 export default function Header() {
-  return <div>Im the header!</div>;
+  const classes = useStyles({});
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            ez URL
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign Up</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
