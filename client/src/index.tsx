eff8792b962a44e83f8736b06e3bd9a3d52d6732
shmeegie <ericellb@gmail.com>
2019-09-29 00:06:07 -0400

    More skeleton + dependancies

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import * as process from 'process';
 
 const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : process.env.REACT_APP_API_URL;
 
