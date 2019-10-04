86e0885452116152f8303a66c918fd0ed7263923
ericellb <ericellb@gmail.com>
2019-10-04 00:28:23 -0400

    Oops no more port, nginx proxying to express app

--- client/src/components/AxiosClient/index.ts
+++ client/src/components/AxiosClient/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://lb.ltng.link:3001';
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://lb.ltng.link';
 
 export default axios.create({
   baseURL: baseUrl
