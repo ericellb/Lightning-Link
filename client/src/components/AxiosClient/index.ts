c6609ec0dd6dc56a337b7d44f2300671e2cb667f
ericellb <ericellb@gmail.com>
2019-10-04 00:26:56 -0400

    Domain for Backend

--- client/src/components/AxiosClient/index.ts
+++ client/src/components/AxiosClient/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://54.81.97.56:3001';
+const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://lb.ltng.link:3001';
 
 export default axios.create({
   baseURL: baseUrl
